ff1e4adafd162a48fcf6808db93a748268439dc2
dan saa <dansaa8@protonmail.com>
2024-09-13 11:57:59 +0200

    Add another option to the menu.

--- PriceCalculator.Helpers/MenuHelpers.cs
+++ PriceCalculator.Helpers/MenuHelpers.cs
@@ -1,13 +1,15 @@
 namespace PriceCalculator.Helpers;
 
-public class MenuHelpers
+public static class MenuHelpers
 {
     public const string Quit = "0";
+    public const string EvaluatePrice = "1";
 
     public static void Print()
     {
         Console.WriteLine("Welcome to Price Calculator!" +
                           "\nNavigate through the different options with the corresponding numbers" +
-                          $"\n{Quit}: Quit");
+                          $"\n{Quit}: Quit" +
+                          $"\n{EvaluatePrice}: Evaluate Price");
     }
 }
